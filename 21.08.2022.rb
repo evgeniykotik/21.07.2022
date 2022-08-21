b184615804eadddc1b492ad4ec01be8b8bff5357
Evgeniykotik <100156071+evgeniykotik@users.noreply.github.com>
2022-08-21 10:15:01 +0300

    Add files via upload

--- 21.08.2022.rb
+++ 21.08.2022.rb
@@ -11,3 +11,44 @@ def get_exponent(n, p)
   
     end
 end
+def switcheroo(x) 
+  x.chars.map{|x| if x=='b'
+  x='a'
+  elsif x=='a'
+  x='b'
+  else x=='c'
+  x
+  end}.join
+end
+def break_chocolate(n, m)
+if n>0 && m>0
+  n*m-1
+  else 
+  0
+  end
+end
+def to_camel_case(str)
+  if str.include?("_")
+    str.split("_").each_with_index.map{|x, i| if i>0 
+      x.capitalize
+    else
+    x
+    end}.join
+    else
+    str.split("-").map{|x| x.capitalize}.join
+end
+  end
+  def contamination(text, char)
+text.chars.map{|x| x=char}.join
+end
+def DNA_strand(dna)
+dna.chars.map{|x| if x=="A"
+x="T"
+elsif x=="T"
+x="A"
+elsif x=="C"
+x="G"
+else x=="G"
+x="C"
+end}.join
+end
